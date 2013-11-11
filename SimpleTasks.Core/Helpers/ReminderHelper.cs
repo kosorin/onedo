2f99a7f97c0d85b4650fa4e3d9cd5d280b249ce8
David Kosorin <david@kosorin.net>
2013-11-11 13:54:57 +0100

    ReminderHelper... zapomněl jsem ho při změně jména přidat :(

---
+++ SimpleTasks.Core/Helpers/ReminderHelper.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Phone.Scheduler;
+using SimpleTasks.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTasks.Core.Helpers
+{
+    public class ReminderHelper
+    {
+        public static void Add(string name, string title, string content, DateTime beginTime)
+        {
+            Remove(name);
+
+            Reminder reminder = new Reminder(name)
+            {
+                BeginTime = beginTime,
+                Title = title,
+                Content = content,
+            };
+
+            try
+            {
+                ScheduledActionService.Add(reminder);
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("Chyba při přidání připomínky.");
+            }
+        }
+        
+        public static bool Exists(string name)
+        {
+            return ScheduledActionService.Find(name) != null;
+        }
+
+        public static void Remove(string name)
+        {
+            if (Exists(name))
+            {
+                ScheduledActionService.Remove(name);
+            }
+        }
+    }
+}
