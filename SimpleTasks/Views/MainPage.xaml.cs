b8e7bb12b198d831cd13160f78f49f5abacb164c
David Kosorin <david@kosorin.net>
2013-11-12 00:47:41 +0100

    Upraveny data pro demonstraci a vytvořeny screenshoty pro všechny jazaky a rozlišení

--- SimpleTasks/Views/MainPage.xaml.cs
+++ SimpleTasks/Views/MainPage.xaml.cs
@@ -106,11 +106,10 @@ namespace SimpleTasks.Views
         void appBarResetMenuItem_Click(object sender, EventArgs e)
         {
             ViewModel.Tasks.Clear();
-            ViewModel.Tasks.Add(new TaskModel() { Title = "Buy milk", DueDate = DateTimeExtensions.Today.AddDays(-7) });
-            ViewModel.Tasks.Add(new TaskModel() { Title = "Book flight to London", DueDate = DateTimeExtensions.Today.AddDays(-2), Priority = TaskPriority.High });
-            ViewModel.Tasks.Add(new TaskModel() { Title = "Very very long task for demonstration, lorem ipsum", DueDate = DateTimeExtensions.Today.AddDays(0) });
+            ViewModel.Tasks.Add(new TaskModel() { Title = "Buy milk", DueDate = DateTimeExtensions.Today.AddDays(-7), Priority = TaskPriority.Low });
+            ViewModel.Tasks.Add(new TaskModel() { Title = "Book flight to London", DueDate = DateTimeExtensions.Today.AddDays(-2), Priority = TaskPriority.High, ReminderDate = DateTime.Now });
             ViewModel.Tasks.Add(new TaskModel() { Title = "Go to the dentist", DueDate = DateTimeExtensions.Today.AddDays(1) });
-            ViewModel.Tasks.Add(new TaskModel() { Title = "Release new version", DueDate = DateTimeExtensions.Today.AddDays(1), Priority = TaskPriority.Low });
+            ViewModel.Tasks.Add(new TaskModel() { Title = "Release new version", DueDate = DateTimeExtensions.Today.AddDays(1), ReminderDate = DateTime.Now });
             ViewModel.Tasks.Add(new TaskModel() { Title = "Math project", DueDate = DateTimeExtensions.Today.AddDays(2), Priority = TaskPriority.High });
             ViewModel.Tasks.Add(new TaskModel() { Title = "Pay the rent", DueDate = DateTimeExtensions.Today.AddDays(4) });
             ViewModel.Tasks.Add(new TaskModel() { Title = "Call Chuck", DueDate = DateTimeExtensions.Today.AddDays(6) });
