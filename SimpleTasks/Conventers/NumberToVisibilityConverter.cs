334e2753fa01eceea25202afce88f3a7178b8132
David Kosorin <david@kosorin.net>
2014-08-09 01:54:41 +0200

    Negate pro Int to Visibility converter

--- SimpleTasks/Conventers/NumberToVisibilityConverter.cs
+++ SimpleTasks/Conventers/NumberToVisibilityConverter.cs
@@ -10,11 +10,23 @@ namespace SimpleTasks.Conventers
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             int parameterValue = 0;
+            bool negate = false;
+
             if (parameter != null && parameter is string)
             {
-                int.TryParse((string)parameter, out parameterValue);
+                string p = (string)parameter;
+                if (p.StartsWith("!"))
+                {
+                    negate = true;
+                    p = p.Substring(1);
+                }
+                int.TryParse(p, out parameterValue);
             }
-            return (value is int && (int)value == parameterValue) ? Visibility.Visible : Visibility.Collapsed;
+
+            if (negate)
+                return (value is int && (int)value == parameterValue) ? Visibility.Collapsed : Visibility.Visible;
+            else
+                return (value is int && (int)value == parameterValue) ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
