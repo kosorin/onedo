536f5af3dff9a2c5c719d60ed654934a94dd4e08
David Kosorin <david@kosorin.net>
2014-08-09 17:23:13 +0200

    ListPickerValueItem

---
+++ SimpleTasks/Controls/ListPickerValueItem.cs
@@ -0,0 +1,27 @@
+﻿using SimpleTasks.Core.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTasks.Controls
+{
+    public class ListPickerValueItem<T> : ListPickerItem
+    {
+        #region Value
+        private T _value;
+        public T Value
+        {
+            get { return _value; }
+            set { SetProperty(ref _value, value); }
+        }
+        #endregion
+
+        public ListPickerValueItem(T value, string label, double opacity = -1) :
+            base(label, opacity)
+        {
+            Value = value;
+        }
+    }
+}
