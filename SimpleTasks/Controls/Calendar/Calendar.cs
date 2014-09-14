287f54d1d256bc469db61cf64d70302fb82dfb15
David Kosorin <david@kosorin.net>
2014-09-15 00:12:22 +0200

    no message

--- SimpleTasks/Controls/Calendar/Calendar.cs
+++ SimpleTasks/Controls/Calendar/Calendar.cs
@@ -278,7 +278,7 @@ namespace SimpleTasks.Controls.Calendar
             {
                 if (calendar._monthItemsGrid != null)
                 {
-                    Point origin = calendar._monthItemsGrid.RenderTransformOrigin;
+                    Point origin = new Point();
                     origin.X = (((calendar.DisplayDate.Month - 1) % _monthColumnCount) / (double)(_monthColumnCount - 1));
                     origin.Y = ((calendar.DisplayDate.Month - 1) / _monthColumnCount) / (double)(_monthRowCount - 1);
                     calendar._monthItemsGrid.RenderTransformOrigin = origin;
