7dd9f07b70bbc4273bd26590cd6ee93bd0f2f00d
David Kosorin <david@kosorin.net>
2015-01-10 15:26:57 +0100

    Zmenšování....

---
+++ SimpleTasks/Controls/BasePivotPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using SimpleTasks.ViewModels;
+using Microsoft.Phone.Shell;
+using SimpleTasks.Resources;
+using SimpleTasks.Helpers;
+
+namespace SimpleTasks.Controls
+{
+    public abstract class BasePivotPage : BasePage
+    {
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            SystemTray.ForegroundColor = (Color)App.Current.Resources["ButtonDarkForegroundColor"];
+        }
+    }
+}
