adcf62227017980ea5a3a34ad695f5b91ba78c1c
David Kosorin <david@kosorin.net>
2015-01-10 21:08:51 +0100

    Změna barev

--- SimpleTasks/Helpers/ThemeHelper.cs
+++ SimpleTasks/Helpers/ThemeHelper.cs
@@ -8,12 +8,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace SimpleTasks.Helpers
 {
     public static class ThemeHelper
     {
+        public static float Lerp(this float start, float end, float amount)
+        {
+            float difference = end - start;
+            float adjusted = difference * amount;
+            return start + adjusted;
+        }
+
+        public static Color Lerp(this Color colour, Color to, float amount)
+        {
+            // start colours as lerp-able floats
+            float sr = colour.R, sg = colour.G, sb = colour.B;
+
+            // end colours as lerp-able floats
+            float er = to.R, eg = to.G, eb = to.B;
+
+            // lerp the colours to get the difference
+            byte r = (byte)sr.Lerp(er, amount),
+                 g = (byte)sg.Lerp(eg, amount),
+                 b = (byte)sb.Lerp(eb, amount);
+
+            // return the new colour
+            return Color.FromArgb(colour.A, r, g, b);
+        }
 
         private static ResourceDictionary Resources
         {
@@ -190,6 +214,18 @@ namespace SimpleTasks.Helpers
             {
                 _appBarBackground = (Color)Resources["SystemTrayBackgroundColor"];
                 _appBarForeground = (Color)Resources["SystemTrayForegroundColor"];
+            }
+
+            // Speciální
+            if (Resources.Contains("DarkerAccentBrush"))
+            {
+                Color color = (Color)Resources["AccentColor"];
+                ((SolidColorBrush)Resources["DarkerAccentBrush"]).Color = color.Lerp(Colors.Black, 0.12f);
+            }
+            if (Resources.Contains("DarkerTasksListHeaderBackgroundBrush"))
+            {
+                Color color = (Color)Resources["TasksListHeaderBackgroundColor"];
+                ((SolidColorBrush)Resources["DarkerTasksListHeaderBackgroundBrush"]).Color = color.Lerp(Colors.Black, 0.1f);
             }
 
 #if DEBUG
