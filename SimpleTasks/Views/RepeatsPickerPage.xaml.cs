ba97ee101a47d6b16cd9ef119d4a2caef64ce23f
David Kosorin <david@kosorin.net>
2015-03-18 22:49:05 +0100

    vzhled repeats pickeru

--- SimpleTasks/Views/RepeatsPickerPage.xaml.cs
+++ SimpleTasks/Views/RepeatsPickerPage.xaml.cs
@@ -26,23 +26,16 @@ namespace SimpleTasks.Views
 
         private static readonly int DayCount = 7;
 
-        private static readonly DayOfWeek FirstDayOfWeek = CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
-
         static RepeatsPickerPage()
         {
             DayNames = new string[DayCount];
-            DayNames[GetIndex(DayOfWeek.Monday)] = CultureInfo.CurrentCulture.DateTimeFormat.DayNames[(int)DayOfWeek.Monday];
-            DayNames[GetIndex(DayOfWeek.Tuesday)] = CultureInfo.CurrentCulture.DateTimeFormat.DayNames[(int)DayOfWeek.Tuesday];
-            DayNames[GetIndex(DayOfWeek.Wednesday)] = CultureInfo.CurrentCulture.DateTimeFormat.DayNames[(int)DayOfWeek.Wednesday];
-            DayNames[GetIndex(DayOfWeek.Thursday)] = CultureInfo.CurrentCulture.DateTimeFormat.DayNames[(int)DayOfWeek.Thursday];
-            DayNames[GetIndex(DayOfWeek.Friday)] = CultureInfo.CurrentCulture.DateTimeFormat.DayNames[(int)DayOfWeek.Friday];
-            DayNames[GetIndex(DayOfWeek.Saturday)] = CultureInfo.CurrentCulture.DateTimeFormat.DayNames[(int)DayOfWeek.Saturday];
-            DayNames[GetIndex(DayOfWeek.Sunday)] = CultureInfo.CurrentCulture.DateTimeFormat.DayNames[(int)DayOfWeek.Sunday];
-        }
-
-        private static int GetIndex(DayOfWeek dayOfWeek)
-        {
-            return ((int)(dayOfWeek - FirstDayOfWeek) + 7) % DayCount;
+            DayNames[(int)DayOfWeek.Monday] = CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedDayNames[(int)DayOfWeek.Monday];
+            DayNames[(int)DayOfWeek.Tuesday] = CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedDayNames[(int)DayOfWeek.Tuesday];
+            DayNames[(int)DayOfWeek.Wednesday] = CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedDayNames[(int)DayOfWeek.Wednesday];
+            DayNames[(int)DayOfWeek.Thursday] = CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedDayNames[(int)DayOfWeek.Thursday];
+            DayNames[(int)DayOfWeek.Friday] = CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedDayNames[(int)DayOfWeek.Friday];
+            DayNames[(int)DayOfWeek.Saturday] = CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedDayNames[(int)DayOfWeek.Saturday];
+            DayNames[(int)DayOfWeek.Sunday] = CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedDayNames[(int)DayOfWeek.Sunday];
         }
         #endregion // end of Static
 
@@ -52,35 +45,27 @@ namespace SimpleTasks.Views
             InitializeComponent();
             DataContext = this;
 
-            List<ListPickerItem<Repeats>> repeatsList = new List<ListPickerItem<Repeats>>();
-            repeatsList.Add(new ListPickerItem<Repeats>(AppResources.RepeatsOnce, Repeats.None));
-            repeatsList.Add(new ListPickerItem<Repeats>(AppResources.RepeatsDaily, Repeats.Daily));
-            repeatsList.Add(new ListPickerItem<Repeats>(AppResources.RepeatsDaysOfWeek, Repeats.Monday));
-            repeatsList.Add(new ListPickerItem<Repeats>(AppResources.RepeatsWeekly, Repeats.Weekly));
-            repeatsList.Add(new ListPickerItem<Repeats>(AppResources.RepeatsMonthly, Repeats.Monthly));
-            RepeatsListPicker.ItemsSource = repeatsList;
-
             Repeats repeats = NavigationParameter<Repeats>(_name, DefaultRepeats);
             switch (repeats)
             {
             case Repeats.Monthly:
-                RepeatsListPicker.SelectedIndex = 4;
+                MonthlyRadioButton.IsChecked = true;
                 break;
 
             case Repeats.Weekly:
-                RepeatsListPicker.SelectedIndex = 3;
+                WeeklyRadioButton.IsChecked = true;
                 break;
 
             case Repeats.Daily:
-                RepeatsListPicker.SelectedIndex = 1;
+                DailyRadioButton.IsChecked = true;
                 break;
 
             case Repeats.None:
-                RepeatsListPicker.SelectedIndex = 0;
+                OnceRadioButton.IsChecked = true;
                 break;
 
-            default /* DaysOfWeek */:
-                RepeatsListPicker.SelectedIndex = 2;
+            default: // DaysOfWeek
+                DaysOfWeekRadioButton.IsChecked = true;
                 break;
             }
 
@@ -89,23 +74,22 @@ namespace SimpleTasks.Views
 
         private bool IsSetDayOfWeek(DayOfWeek dayOfWeek)
         {
-            return ((ToggleButton)ButtonPanel.Children[GetIndex(dayOfWeek)]).IsChecked ?? false;
+            return ((ToggleButton)ButtonPanel.Children[(int)dayOfWeek]).IsChecked ?? false;
         }
 
         private void SetDayOfWeek(DayOfWeek dayOfWeek, bool value)
         {
-            ((ToggleButton)ButtonPanel.Children[GetIndex(dayOfWeek)]).IsChecked = value;
+            ((ToggleButton)ButtonPanel.Children[(int)dayOfWeek]).IsChecked = value;
         }
 
         protected override object Save()
         {
-            switch (RepeatsListPicker.SelectedIndex)
+            if (DailyRadioButton.IsChecked ?? false)
             {
-            case 4: return Repeats.Monthly;
-
-            case 3: return Repeats.Weekly;
-
-            case 2:
+                return Repeats.Daily;
+            }
+            else if (DaysOfWeekRadioButton.IsChecked ?? false)
+            {
                 Repeats repeats = Repeats.None;
                 if (IsSetDayOfWeek(DayOfWeek.Monday)) repeats |= Repeats.Monday;
                 if (IsSetDayOfWeek(DayOfWeek.Tuesday)) repeats |= Repeats.Tuesday;
@@ -115,12 +99,17 @@ namespace SimpleTasks.Views
                 if (IsSetDayOfWeek(DayOfWeek.Saturday)) repeats |= Repeats.Saturday;
                 if (IsSetDayOfWeek(DayOfWeek.Sunday)) repeats |= Repeats.Sunday;
                 return repeats;
-
-            case 1: return Repeats.Daily;
-
-            case 0:
-            default: return Repeats.None;
             }
+            else if (WeeklyRadioButton.IsChecked ?? false)
+            {
+                return Repeats.Weekly;
+            }
+            else if (MonthlyRadioButton.IsChecked ?? false)
+            {
+                return Repeats.Monthly;
+            }
+
+            return Repeats.None;
         }
 
         private void UpdateButtons(Repeats repeats)
@@ -132,18 +121,6 @@ namespace SimpleTasks.Views
             SetDayOfWeek(DayOfWeek.Friday, (repeats & Repeats.Friday) != 0);
             SetDayOfWeek(DayOfWeek.Saturday, (repeats & Repeats.Saturday) != 0);
             SetDayOfWeek(DayOfWeek.Sunday, (repeats & Repeats.Sunday) != 0);
-        }
-
-        private void RepeatsListPicker_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (RepeatsListPicker.SelectedItem != null)
-            {
-                ListPickerItem<Repeats> item = RepeatsListPicker.SelectedItem as ListPickerItem<Repeats>;
-                if (item != null)
-                {
-                    DaysOfWeekContainer.IsChecked = item.Value == Repeats.Monday;
-                }
-            }
         }
     }
 }
