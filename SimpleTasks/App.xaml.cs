99e52186fa09120c8ce280a6d58349b4acb31781
David Kosorin <david@kosorin.net>
2015-03-19 10:39:54 +0100

    Překlady + verze 4.9

--- SimpleTasks/App.xaml.cs
+++ SimpleTasks/App.xaml.cs
@@ -27,7 +27,7 @@ namespace SimpleTasks
 {
     public partial class App : Application
     {
-        public static string ForceDebugCulture = "en-US";
+        public static string ForceDebugCulture = "es-ES";
 
         #region Properties
         public static readonly string BackgroundAgentName = "PeriodicBackgroundAgent";
